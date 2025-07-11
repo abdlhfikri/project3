45d24d060514f1ba4b3bf20d5eb342908311b738
abdlhfikri <abdlhfikricampus@gmail.com>
2025-07-11 11:38:17 +0700

    Menambah isian view

---
+++ app/Views/contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+<div class="container">
+        <div class="row">
+            <div class="col-md-12 my-2 card">
+                <div class="card-body">
+                    <h5 class="h5">Alamat</h5>
+                    <p>Lorem ipsum dolor sit
+amet consectetur p>
+                </div>
+            </div>
+            <div class="col-md-12 my-2 card">
+                <div class="card-body">
+                    <h5 class="h5">Email</h5>
+                    <p>Lorem ipsum dolor sit
+amet</p>
+                </div>
+            </div>
+            <div class="col-md-12 my-2 card">
+                <div class="card-body">
+                    <h5 class="h5">No.HP</h5>
+                    <p>Lorem ipsum dolor sit
+</p>
+                </div>
+            </div>
+        </div>
+</div>
+</body>
+</html>
---
+++ app/Views/faqs.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faqs</title>
+</head>
+<body>
+<div class="container">
+        <div class="row">
+            <div class="col-md-12 my-2 card">
+                <div class="card-body">
+                    <h5 class="h5">Pertanyaan?
+(Q)</h5>
+                    <p>(A) Lorem ipsum dolor
+sit amet consectetur adipisicing elit. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
